c18daab4ff35f3dc3d8981a78a19103d8f93ee02
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-09-21 16:37:38 -0400

    Initial commit

--- Assignment 1/Computer.java
+++ Assignment 1/Computer.java
@@ -1,3 +1,12 @@
-public class Computer{
+ class Computer{
 
+	 private String brand;
+	 private String model;
+	 private long SN;
+     private double price;	 
+     
+     public static void main(String[] args ){
+       
+
+     }
 }

784acd880f84ada655d4ec4f8b422e040c380671
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-09-21 22:18:44 -0400

    Added setters and getters(Accessors and Mutators)

--- Assignment 1/Computer.java
+++ Assignment 1/Computer.java
@@ -5,8 +5,38 @@
 	 private long SN;
      private double price;	 
      
-     public static void main(String[] args ){
-       
+	 public String getBrand() {
+		return brand;
+	}
 
-     }
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public long getSN() {
+		return SN;
+	}
+
+	public void setSN(long sN) {
+		SN = sN;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+     
+    
 }

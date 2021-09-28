ba507e6b4ba67bb9611f53b49ca45fcb46542530
Dhananjay Narayan <dhananjayashwath@gmail.com>
2021-09-28 03:30:15 -0400

    Password check fn done

--- Assignment 1/Driver.java
+++ Assignment 1/Driver.java
@@ -7,40 +7,70 @@ import java.util.*;
 // ----------------------------------------------------- 
 
 public class Driver {
-
+	public static String password = "password";
+	public static Scanner sc= new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		String password = "password";
 		
-		System.out.println(password);
-		
-		// TODO Auto-generated method stub
 		System.out.println("=============================");
         System.out.println("Welcome to the Computer Store");
 		System.out.println("=============================");
 		System.out.println();
 		
-		System.out.println("Enter the maximum number of Computers your store can have: ");
-		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the maximum number of Computers your store can have: \n");
+		
 		int maxComputers = sc.nextInt();
 		
 		Computer[] inventory = new Computer[maxComputers];
 		
 		while(true) {
-			System.out.println("What do you want to do?");
+			System.out.println("What do you want to do?\n");
 			System.out.println("1. Enter new computers (password required)");
 			System.out.println("2. Change information of a computer (password required)");
 			System.out.println("3. Display all computers by a specific brand");
 			System.out.println("4. Display all computers under a certain a price.");
-			System.out.println("5. Quit");
+			System.out.println("5. Quit\n");
 			System.out.println("Please enter your choice >");
 			
 			int choice = sc.nextInt();
 			
 			switch(choice) {
 			
+			case 1: 
+				
+				System.out.println("Enter the password. You will have a max of 3 chances.");
+				boolean passwordStatus = passwordCheck();
+				
+				if(passwordStatus==false) {
+					break;
+				}
+				System.out.println("How many computers do you want to add in the inventory?");
+				int noOfCompToAdd = sc.nextInt();
+				
+				
 			}
 		}
 		
+	}
+
+	public static boolean passwordCheck() {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=3;i++) {
+			System.out.println("Attempt "+i+" : ");
+			
+			System.out.println("Enter the password:");
+			String enteredPassword = sc.next()	;
+			
+			if(enteredPassword.equals(password)) {
+				System.out.println("You are now Logged In.");
+				return true;
+			}
+			else {
+				System.out.println("Incorrect Password!!!");
+				continue;				
+			}			
+			}		
+		return false;
 	}
 
 }
